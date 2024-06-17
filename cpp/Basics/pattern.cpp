4423f616a5178b0c476f6969643f5687f1f234f4
Shubhankar_Ranjan <shubhankar515@gmail.com>
2024-06-17 14:42:29 +0530

    refactor: improve pattern printing logic and readability

--- cpp/Basics/pattern.cpp
+++ cpp/Basics/pattern.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
 
-    for(int i = 1; i <= 4; i++){
-        for(int j = i; j >= 1; j--){
-            cout << j << "\t";
+    int i = 1;
+    while (i <= n)
+    {
+        int j = n;
+
+        while (j >= i)
+        {
+            cout << n - j + 1 << "\t";
+            j--;
         }
+        j = n;
+        int spaces = 1;
+        while (spaces <= 2 * i - 2)
+        {
+            cout << "*" << "\t";
+            spaces++;
+        }
+        while (j >= i)
+        {
+            cout << j - i + 1 << "\t";
+            j--;
+        }
+
         cout << endl;
+        i += 1;
     }
 
     return 0;
